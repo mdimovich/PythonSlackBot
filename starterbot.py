eee62b70346b1ed316ac20b2c218d2313df1d00a
Michael <mdimovich@gmail.com>
2017-01-01 01:10:07 -0500

    Minor changes

--- starterbot.py
+++ starterbot.py
@@ -1,25 +1,17 @@
+# Python Slack Bot: Michael Dimovich 
 import os
 import time
 from slackclient import SlackClient
 
 
-# starterbot's ID as an environment variable
 BOT_ID = os.environ.get("BOT_ID")
-
-# constants
 AT_BOT = "<@" + BOT_ID + ">"
 EXAMPLE_COMMAND = "do"
-
-# instantiate Slack & Twilio clients
 slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
 
-
+# Handle the various commands 
+# Figure out a way to do arithmetic operations and send back to slack channel
 def handle_command(command, channel):
-    """
-        Receives commands directed at the bot and determines if they
-        are valid commands. If so, then acts on the commands. If not,
-        returns back what it needs for clarification.
-    """
     response = "Not sure what you mean. Use the *" + EXAMPLE_COMMAND + "* command with numbers, delimited by spaces."
     if command.startswith(EXAMPLE_COMMAND):
         response = "Sure...write some more code then I can do that!"
@@ -30,25 +22,20 @@ def handle_command(command, channel):
     if command == "do list users":
         slack_client.api_call("chat.postMessage", channel="#botpractice", text="Users are...Unknown " , as_user=True)
 
-
+# Return formatted text after messaging bot
 def parse_slack_output(slack_rtm_output):
-    """
-        The Slack Real Time Messaging API is an events firehose.
-        this parsing function returns None unless a message is
-        directed at the Bot, based on its ID.
-    """
     output_list = slack_rtm_output
     if output_list and len(output_list) > 0:
         for output in output_list:
             if output and 'text' in output and AT_BOT in output['text']:
-                # return text after the @ mention, whitespace removed
                 return output['text'].split(AT_BOT)[1].strip().lower(), output['channel']
     return None, None
 
 
 READ_WEBSOCKET_DELAY = 1
 if slack_client.rtm_connect():
-    print("StarterBot connected and running!")
+    print("StarterBot connected and running")
+    # Practice call to verify channel connection
     slack_client.api_call("chat.postMessage", channel="#botpractice", text="Bot up and running")
     while True:
         command, channel = parse_slack_output(slack_client.rtm_read())
