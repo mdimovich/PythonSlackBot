abb767338a3e46d2b54a9eddba6c162df4125fef
Michael <mdimovich@gmail.com>
2017-01-01 06:14:40 -0500

    Implemented for functions for the bot to use
    Added multiple functions, most of them not fully functional yet. Need to find a way to parse the JSON from users.list and output it back to the Slack channel.

--- starterbot.py
+++ starterbot.py
@@ -1,41 +1,62 @@
-# Python Slack Bot: Michael Dimovich 
 import os
 import time
 from slackclient import SlackClient
+import json
 
 
+# Note: The BOT_ID has to be an environment variable for this to work
 BOT_ID = os.environ.get("BOT_ID")
+
+
 AT_BOT = "<@" + BOT_ID + ">"
 EXAMPLE_COMMAND = "do"
+
+# Grabs Authentication Token. Needs to be an environment variable.
 slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
 
-# Handle the various commands 
-# Figure out a way to do arithmetic operations and send back to slack channel
+
 def handle_command(command, channel):
     response = "Not sure what you mean. Use the *" + EXAMPLE_COMMAND + "* command with numbers, delimited by spaces."
     if command.startswith(EXAMPLE_COMMAND):
-        response = "Sure...write some more code then I can do that!"
-    slack_client.api_call("chat.postMessage", channel="#botpractice",
+        response = "Processing Command..."
+    slack_client.api_call("chat.postMessage", channel=channel,
                           text=response, as_user=True)
-    if command.find("add") != -1:
-        slack_client.api_call("chat.postMessage", channel="#botpractice", text="What would you like to add?", as_user=True)
+    # Function for adding. Not sure how to implement yet.
+    if command == "do add":
+        slack_client.api_call("chat.postMessage", channel=channel, text="What would you like to add? :tada:", as_user=True)
+        slack_client.api_call()
     if command == "do list users":
-        slack_client.api_call("chat.postMessage", channel="#botpractice", text="Users are...Unknown " , as_user=True)
+        parsed_users = json.loads("users.list")
+       # print(parsed_users)
+        slack_client.api_call("chat.postMessage", channel=channel, text="Some text testing 1,2")
+    # Sends a thumbs up emoji message
+    if command == "do thumbs up":
+        slack_client.api_call("reactions.add", channel=channel, name="thumbsup", timestamp="1234567890.123456")
+        slack_client.api_call("chat.postMessage", channel=channel, text=":thumbsup:", as_user=True)
+    if command == "do list channels":
+        slack_client.api_call("channel.list")
+    # For uploading files, but doesn't work yet
+    if command == "do upload file":
+        slack_client.api_call("files.upload", filename="file.txt", file="multipart/form-data", channel=channel)
+    # Test function that tests all of the different postMessage capabilities.
+    # Note: Attachments is in JSON format
+    if command == "do test":
+        slack_client.api_call("chat.postMessage",icon_emoji=":smile:", channel=channel, attachments=[{"pretext": "Hey there","title": "Testing Section", "color": "good", "text": "This is the text"}])
 
-# Return formatted text after messaging bot
 def parse_slack_output(slack_rtm_output):
     output_list = slack_rtm_output
     if output_list and len(output_list) > 0:
         for output in output_list:
             if output and 'text' in output and AT_BOT in output['text']:
+                # return text after the @ mention, whitespace removed
                 return output['text'].split(AT_BOT)[1].strip().lower(), output['channel']
     return None, None
 
 
 READ_WEBSOCKET_DELAY = 1
 if slack_client.rtm_connect():
-    print("StarterBot connected and running")
-    # Practice call to verify channel connection
+    print("StarterBot up and running!")
+    # Test Message to make sure bot can post to channel #botpractice
     slack_client.api_call("chat.postMessage", channel="#botpractice", text="Bot up and running")
     while True:
         command, channel = parse_slack_output(slack_client.rtm_read())
